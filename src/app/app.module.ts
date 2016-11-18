614875021e5f97565621f7d300e7ee87268a0544
Bambe Chen <ayuha406645@gmail.com>
2016-11-18 14:45:51 +0800

    home/home2 sub component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { SecondComponent } from './second/second.component';
 import { Home1Component } from './home/home1/home1.component';
+import { Home2Component } from './home/home2/home2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     SecondComponent,
-    Home1Component
+    Home1Component,
+    Home2Component
   ],
   imports: [
     BrowserModule,
