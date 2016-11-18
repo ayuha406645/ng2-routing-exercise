3f0e14ccfa9613397101b1540e96ecdbb3fc1950
Bambe Chen <ayuha406645@gmail.com>
2016-11-18 16:32:29 +0800

    sub children routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,24 @@ import { Routes, Route, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SecondComponent } from './second/second.component';
 
+import { Home1Component } from './home/home1/home1.component';
+import { Home2Component } from './home/home2/home2.component';
+import { HomeDetailComponent } from './home/home-detail/home-detail.component';
+
 const fullbackRoute: Route = {
   path: '**', component: HomeComponent
 };
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent,
+    children: [
+      { path: '', redirectTo: 'home1' },
+      { path: 'home1', component: Home1Component },
+      { path: 'home2', component: Home2Component },
+      { path: ':id', component: HomeDetailComponent }
+    ]
+  },
   { path: 'second', component: SecondComponent },
   fullbackRoute
 ];
@@ -17,6 +29,6 @@ const routes: Routes = [
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
-  providers: []
+  providers: [],
 })
 export class AppRoutingModule { }
